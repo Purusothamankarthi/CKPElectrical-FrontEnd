f19346bf7242382a3fd9d0bf7724b9283376e863
Purusothamankarthi <purusothaman9952@gmail.com>
2026-08-03 07:51:05 +0530

    Update backend URL to correct live URL electrial-backend.onrender.com

--- api.js
+++ api.js
@@ -1,6 +1,6 @@
 const SERVER_URL = window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1"
     ? "http://localhost:8080"
-    : "https://ckpelectrical-backend.onrender.com";
+    : "https://electrial-backend.onrender.com";
 
 
 function apiFetch(url, options = {}) {
